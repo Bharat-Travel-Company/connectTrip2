ea0ca0b5c49cd03590b83a1af20842dfcc52a45c
thisiskannamma <MY_NAME@example.com>
2024-11-23 15:58:24 +0530

    added gtm id

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import TagManager from 'react-gtm-module'
+
+const tagManagerArgs = {
+  gtmId: 'GTM-5BPH48RC', // Replace with your GTM ID
+}
+
+TagManager.initialize(tagManagerArgs)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
